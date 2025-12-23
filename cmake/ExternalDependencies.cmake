236bd1fb403b6a875c1761fa4463aa9d7662e204
burgholzer <burgholzer@me.com>
2025-12-23 18:21:21 +0100

    📌 explicitly pin to the `v3.3.3` commit of mqt-core

--- cmake/ExternalDependencies.cmake
+++ cmake/ExternalDependencies.cmake
@@ -88,11 +88,11 @@ else()
 endif()
 
 # cmake-format: off
-set(MQT_CORE_MINIMUM_VERSION 3.1.0
+set(MQT_CORE_MINIMUM_VERSION 3.3.3
     CACHE STRING "MQT Core minimum version")
 set(MQT_CORE_VERSION 3.3.3
     CACHE STRING "MQT Core version")
-set(MQT_CORE_REV "v3.3.3"
+set(MQT_CORE_REV "8c9f6ab24968401e450812fc0ff7d05b5ae07a63"
     CACHE STRING "MQT Core identifier (tag, branch or commit hash)")
 set(MQT_CORE_REPO_OWNER "munich-quantum-toolkit"
     CACHE STRING "MQT Core repository owner (change when using a fork)")

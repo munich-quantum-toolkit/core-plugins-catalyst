55ff3ee2dcb9b2a9115b8b9e1fa05fe1cb749ef4
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2026-06-21 23:11:00 +0200

    🔧 Maintenance round (#157)
    ## Description
    
    This PR makes several maintenance updates:
    
    - Fix `required-checks-pass` job
    - Use `client-id` instead of `app-id`
    - Update to new MQSC branding
    - Improve support for `CLAUDE.md`
    
    ## Checklist
    
    - [x] The pull request only contains commits that are focused and
    relevant to this change.
    - [x] ~~I have added appropriate tests that cover the new/changed
    functionality.~~
    - [x] ~~I have updated the documentation to reflect these changes.~~
    - [x] ~~I have added entries to the changelog for any noteworthy
    additions, changes, fixes, or removals.~~
    - [x] ~~I have added migration instructions to the upgrade guide (if
    needed).~~
    - [x] The changes follow the project's style guidelines and introduce no
    new warnings.
    - [x] The changes are fully tested and pass the CI checks.
    - [x] I have reviewed my own code changes.

--- docs/conf.py
+++ docs/conf.py
@@ -47,25 +47,25 @@ except ImportError:
 release = version.split("+")[0]
 
 project = "MQT Core Catalyst Plugin"
-author = "Chair for Design Automation, TUM & Munich Quantum Software Company"
+author = "Chair for Design Automation, TUM & Munich Quantum Software Company GmbH"
 language = "en"
-project_copyright = "2025 - 2026 Chair for Design Automation, TUM & 2025 - 2026 Munich Quantum Software Company"
+project_copyright = "2025 - 2026 Chair for Design Automation, TUM & 2025 - 2026 Munich Quantum Software Company GmbH"
 
 master_doc = "index"
 
 templates_path = ["_templates"]
 
 extensions = [
-    "myst_nb",
     "autoapi.extension",
+    "myst_nb",
+    "sphinx_copybutton",
+    "sphinx_design",
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
     "sphinx.ext.napoleon",
-    "sphinx_copybutton",
-    "sphinx_design",
-    "sphinxext.opengraph",
     "sphinx.ext.viewcode",
     "sphinxcontrib.bibtex",
+    "sphinxext.opengraph",
 ]
 
 source_suffix = [".rst", ".md"]
@@ -153,6 +153,7 @@ napoleon_google_docstring = True
 napoleon_numpy_docstring = False
 
 # -- Options for HTML output -------------------------------------------------
+
 html_theme = "furo"
 html_static_path = ["_static"]
 html_css_files = [
